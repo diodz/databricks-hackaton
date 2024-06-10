9299736522f7a83544d478518c37223b114f8309
nikamanthab <nikamanthab>
2024-06-10 21:28:28 +0000

    pdf

--- src/pdf_extractor.py
+++ src/pdf_extractor.py
@@ -2,6 +2,7 @@ def get_text_from_pdf(path):
     from pypdf import PdfReader 
     reader = PdfReader(path) 
     page = reader.pages[0] 
+    text = ''
     for page in reader.pages: 
         text += page.extract_text() 
     return text
